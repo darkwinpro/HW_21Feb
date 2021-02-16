0172a0316f540973ff3439c2b50ea81f7a62f1d2
Darkwin <Darkwin.pro@gmail.com>
2021-02-16 23:21:53 +0300

    shootColldown

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -12,14 +12,26 @@ public class Gun : MonoBehaviour
 
     [SerializeField] private AudioSource _shoot;
     
+    [SerializeField] private float _shootCooldwn = 0.5f;
+    
+    private float _nextTimeShot;
 
     void Update()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            GameObject newBullet = Instantiate(_bulletPrefab, transform.position, transform.rotation);
-            newBullet.GetComponent<Rigidbody>().velocity = transform.forward * _bulletVelocity;
-            _shoot.Play();
+            if (Time.time >= _nextTimeShot )
+            {
+                _nextTimeShot = Time.time + _shootCooldwn;
+                CreateBullet();
+            }
         }
+    }
+
+    private void CreateBullet()
+    {
+        GameObject newBullet = Instantiate(_bulletPrefab, transform.position, transform.rotation);
+        newBullet.GetComponent<Rigidbody>().velocity = transform.forward * _bulletVelocity;
+        _shoot.Play();
     }
 }
